3ab39fadc9e11a0bae67e430444ca6d22ef54607
aray <aray@netspi.com>
2021-06-28 23:44:30 +0530

    no data found

--- src/jobs/JobsList.js
+++ src/jobs/JobsList.js
@@ -3,17 +3,20 @@ import {useDispatch, useSelector} from 'react-redux';
 import {fetchJobs} from '../job/jobAction';
 import JobThumbNail from '../jobThumbnail/JobThumbNail';
 import classes from './jobsList.scss';
-
+import clsx from 'clsx';
 const JobList = () => {
     const jobs = useSelector((state) => state.job.jobs);
+    const loading = useSelector((state) => state.job.loading);
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchJobs());
     }, [dispatch]);
+    
+    if (!jobs?.length && !loading) return <div className={clsx(classes.container, classes.emptyContainer)}>No Data found</div>;
 
     return (
         <div className={classes.container}>
-            {jobs.map(j => <JobThumbNail key={j._id} job={j}/>)}
+            {jobs?.map(j => <JobThumbNail key={j._id} job={j}/>)}
         </div>
     )
 };
